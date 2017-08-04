4a7c6c807dc0c79ef6660c7e6264ee0302f6adbe
Lucas P <lucas.panta@gmail.com>
2017-08-04 18:57:20 +0200

    fix plugin install

--- elastic-stack/amazon-backup/entrypoint.sh
+++ elastic-stack/amazon-backup/entrypoint.sh
@@ -8,17 +8,18 @@ ELASTIC_S3_BACKUP_ACCESS_KEY="your-s3-key"
 chown -R elasticsearch:elasticsearch /var/log
 chown -R elasticsearch:elasticsearch /var/lib/elasticsearch
 
+# re/install repository-s3 plugin
+/usr/share/elasticsearch/bin/elasticsearch-plugin install -b repository-s3
+# restore rights on cron
+chmod 0644 /etc/cron.d/elastic_db_backup
+
 if [ ! "$(ls -A /var/lib/elasticsearch)" ]; then
     # install dependencies
     apt-get install -y curl nodejs cron
-    # install repository-s3 plugin
-    /usr/share/elasticsearch/bin/elasticsearch-plugin install -b repository-s3
     # create the Elastic repository-s3 configuration
     service elasticsearch start
     sleep 10
-    curl -XPUT 'http://localhost:9200/_snapshot/s3_backup?pretty' -H 'Content-Type: application/json' -d'{"type": "s3","settings": {"bucket": "'${ELASTIC_S3_BACKUP_BUCKET}'","region": "'${ELASTIC_S3_BACKUP_REGION}'", "protocol": "https", "secret_key": "'${ELASTIC_S3_BACKUP_SECRET_KEY}'", "access_key": "'${ELASTIC_S3_BACKUP_ACCESS_KEY}'"}}' && \
-    # enable cron
-    chmod 0644 /etc/cron.d/elastic_db_backup
+    curl -XPUT 'http://localhost:9200/_snapshot/s3_backup?pretty' -H 'Content-Type: application/json' -d'{"type": "s3","settings": {"bucket": "'${ELASTIC_S3_BACKUP_BUCKET}'","region": "'${ELASTIC_S3_BACKUP_REGION}'", "protocol": "https", "secret_key": "'${ELASTIC_S3_BACKUP_SECRET_KEY}'", "access_key": "'${ELASTIC_S3_BACKUP_ACCESS_KEY}'"}}'
 else
     service elasticsearch start
 fi
